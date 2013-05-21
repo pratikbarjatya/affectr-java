95ccef0fa2e29b7fc0969fd3911eaeaeffd0e033
Mathias Bogaert <mathias.bogaert@gmail.com>
2013-05-21 13:49:47 +0200

    Initial entry.

--- src/main/java/io/theysay/client/Example.java
+++ src/main/java/io/theysay/client/Example.java
@@ -2,8 +2,8 @@ package io.theysay.client;
 
 public class Example {
     public static void main(String[] args) {
-        Affectr.accountDetails.setUsername("test");
-        Affectr.accountDetails.setPassword("test");
+        Affectr.accountDetails.setUsername("your username");
+        Affectr.accountDetails.setPassword("your password");
 
         System.out.println(Affectr.api.analyseSentiment("this is a test").getSentiment().getLabel());
         System.out.println(Affectr.api.analyseSpeculation("I might buy an ipad")[0].getSpeculationType());
